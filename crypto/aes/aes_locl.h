2156b7e7f7c5d50b7bff8b951b5ce700cd32478a
levitte <>
2002-06-27 13:49:52 +0000

    Make sure that moving 32-bit values will work on Win64 even if they aren't properly aligned.

--- crypto/aes/aes_locl.h
+++ crypto/aes/aes_locl.h
@@ -63,10 +63,15 @@
 #include <string.h>
 
 #ifdef _MSC_VER
-# define SWAP(x) (_lrotl(x, 8) & 0x00ff00ff | _lrotr(x, 8) & 0xff00ff00)
-# define GETU32(p) SWAP(*((u32 *)(p)))
-# define PUTU32(ct, st) { *((u32 *)(ct)) = SWAP((st)); }
-#else
+# ifdef _WIN64 /* little-endian, strict alignment */
+#  define GETU32(pt) (((u32)(pt)[3] << 24) ^ ((u32)(pt)[2] << 16) ^ ((u32)(pt)[1] <<  8) ^ ((u32)(pt)[0]))
+#  define PUTU32(ct, st) { (ct)[3] = (u8)((st) >> 24); (ct)[2] = (u8)((st) >> 16); (ct)[1] = (u8)((st) >>  8); (ct)[0] = (u8)(st); }
+# else /* little-endian, no strict-alignment */
+#  define SWAP(x) (_lrotl(x, 8) & 0x00ff00ff | _lrotr(x, 8) & 0xff00ff00)
+#  define GETU32(p) SWAP(*((u32 *)(p)))
+#  define PUTU32(ct, st) { *((u32 *)(ct)) = SWAP((st)); }
+# endif
+#else /* big-endian, strict alignment */ 
 # define GETU32(pt) (((u32)(pt)[0] << 24) ^ ((u32)(pt)[1] << 16) ^ ((u32)(pt)[2] <<  8) ^ ((u32)(pt)[3]))
 # define PUTU32(ct, st) { (ct)[0] = (u8)((st) >> 24); (ct)[1] = (u8)((st) >> 16); (ct)[2] = (u8)((st) >>  8); (ct)[3] = (u8)(st); }
 #endif

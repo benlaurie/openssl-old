fea6df48f37dfcbb5d82083cd6e1e5ca1f34b904
levitte <>
2002-10-11 22:05:15 +0000

    Latest changes from main trunk

--- apps/openssl.c
+++ apps/openssl.c
@@ -151,11 +151,11 @@ typedef char ** Argv_t;
 #endif
 
 static int do_cmd(LHASH *prog,int argc,char *argv[]);
-CONF *config=NULL;
 char *default_config_file=NULL;
 
 /* Make sure there is only one when MONOLITH is defined */
 #ifdef MONOLITH
+CONF *config=NULL;
 BIO *bio_err=NULL;
 #endif
 
